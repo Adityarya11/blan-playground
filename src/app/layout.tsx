16225dfb7f8bf8f5146cf2fd8293506f271e0562
Adityarya11 <arya050411@gmail.com>
2026-06-25 11:06:13 +0530

    added layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -17,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={`${inter.className} min-h-screen flex flex-col antialiased`}>
         <Navbar />
         <main className="flex-grow flex flex-col">
