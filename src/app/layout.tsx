c92a9722a61d3462462dd0303c20d3bc82d90acd
Adityarya11 <arya050411@gmail.com>
2026-07-06 12:43:34 +0530

    fixed layout for some more space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Blan_Playground",
   description: "The official playground for the Bad Language Compiler.",
+
+  other: {
+    "google-adsense-account": "ca-pub-6447833290642232"
+  }
 };
 
 export default function RootLayout({
@@ -17,10 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4639258361209663"
-          crossOrigin="anonymous"></Script>
+
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6447833290642232"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body className={`${inter.className} min-h-screen flex flex-col antialiased`}>
         <Navbar />
