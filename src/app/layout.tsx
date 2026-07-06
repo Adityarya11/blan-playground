f0d6cc37a675702004e055aeec28ef32e7fc11c2
Adityarya11 <arya050411@gmail.com>
2026-07-06 12:36:21 +0530

    fixed layout for more space.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Blan Playground",
+  title: "Blan_Playground",
   description: "The official playground for the Bad Language Compiler.",
 };
 
@@ -16,7 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
+      <head>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4639258361209663"
+          crossOrigin="anonymous"></Script>
+      </head>
       <body className={`${inter.className} min-h-screen flex flex-col antialiased`}>
         <Navbar />
         <main className="flex-grow flex flex-col">
